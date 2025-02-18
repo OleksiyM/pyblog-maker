9938d31e0b02f3168042d658d64266036850e8f4
OleksiyM <oleksiymaliovanyy@gmail.com>
2025-02-18 18:51:05 +0200

    codo update

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 BLOG_TITLE = "My Blog"  # Blog title
 AUTHOR = "Your Name"     # Default author
-SITE_URL = "https://yourblog.com"  # Replace with your actual domain
+SITE_URL = "http://localhost:3000" # Replace with your actual domain
 OUTPUT_DIR_FORMAT = "%Y-%m-%d_%H-%M-%S" # Date-time format
 POSTS_PER_PAGE = 10
 RECENT_POSTS_COUNT = 3
